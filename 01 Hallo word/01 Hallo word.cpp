3c3e1412986058101fa7748774729bb636be5a2b
cful-chn <103881568+cful-chn@users.noreply.github.com>
2022-11-16 18:51:56 +0100

    hallo word

---
+++ 01 Hallo word/01 Hallo word.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+/*
+main是一个程序的入口
+*/
+
+#define day 7
+int main()
+{
+	const int a = 10;
+	cout << "ein woch hat : " << a << endl;
+
+	system("pause");
+
+	return 0;
+}
